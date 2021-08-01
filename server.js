b66796c58e328f99afd0f2671a563727c4165374
Leigh Dragoon <ldragoon@users.noreply.github.com>
2021-08-01 07:06:19 +0000

    Chained fetches to get authorID

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const parser = require('fast-xml-parser')
 require('dotenv').config()
 
 const key = process.env.API_KEY
-const base_url = 'https://www.goodreads.com/search.xml?key=' + key + '&q='
+const base_url = 'https://www.goodreads.com/author/list.xml?key=' + key + '&id=3389&q='
 
 app.use(cors())
 
@@ -23,28 +23,38 @@ app.get('/', (req, res) => {
 
 app.get('/:query', (req, res, next) => {
   let query = encodeURI(req.params.query)
+  
+  // get author id so you can use it to return
+  // the nicely paginated results
   let url = base_url + query
-
-  console.log(`url: ${url}`)
   
   fetch(url)
     .then(res => res.text())
     .then(xml => {
-      if ( parser.validate(xml) === true ) {
-        // optional (it'll return an object in case it's not valid)
-        const data = parser.parse(xml, {
-          attributeNamePrefix : '',
-          attrNodeName: false,
-          textNodeName: 'text',
-          trimValues: true,
-          arrayMode: false
+      const json = parser.parse(xml)
+      const authorId = json.GoodreadsResponse.author.id
+      const authorIdUrl = 'https://www.goodreads.com/author/list.xml?key=' + key + '&id=' + authorId
+      
+      fetch(authorIdUrl)
+        .then(res => res.text())
+        .then(xml => {
+          if ( parser.validate(xml) === true ) {
+            // optional (it'll return an object in case it's not valid)
+            const data = parser.parse(xml, {
+              attributeNamePrefix : '',
+              attrNodeName: false,
+              textNodeName: 'text',
+              trimValues: true,
+              arrayMode: false
+            })
+    
+            if ( !data ) data = {
+              "error": "this item doesn't exist"
+            }
+            res.send(data)
+          }
         })
-
-        if ( !data ) data = {
-          "error": "this item doesn't exist"
-        }
-        res.send(data)
-      }      
+        .catch(err => console.error(err))
     })
     .catch(err => console.error(err))
 })
